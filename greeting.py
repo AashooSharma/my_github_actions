3a0d101d728b25cd28f8f57aa5df99364bbf2685
As Hacker <65814523+AashooSharma@users.noreply.github.com>
2023-09-16 00:04:39 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,3 +1,15 @@
 # greeting.py
-name = input("Enter your name: ")
+#name = input("Enter your name: ")
+#print(f"Hello dear {name}!")
+
+# greeting.py
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python greeting.py <name>")
+    sys.exit(1)
+
+name = sys.argv[1]
 print(f"Hello dear {name}!")
+
+
